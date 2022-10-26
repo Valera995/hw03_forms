02f04491dce259234c7075cf98303575362506a2
Валерий Егоров <valera995@bk.ru>
2022-10-26 23:19:45 +0300

    Проект новые записи

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -4,18 +4,10 @@ from django.views.generic.base import TemplateView
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Привет, я автор'
-
-        return context
-
 
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Технологии'
 
-        return context
+class About(TemplateView): 
+    template_name = 'about/index.html'
